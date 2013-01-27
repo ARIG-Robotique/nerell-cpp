2a11e2cc662e2677ad7be27e685ab844efc81f78
Gregory DEPUILLE <gregory.depuille@gmail.com>
2013-01-27 18:33:16 +0100

    Configuration du projet avec la nature C++.

---
+++ define.h
@@ -0,0 +1,16 @@
+/*
+ * define.h
+ *
+ *  Created on: 27 janv. 2013
+ *      Author: mythril
+ */
+
+#ifndef DEFINE_H_
+#define DEFINE_H_
+
+#define VERSION			1
+
+// Mode debug : 1 => ON ; 0 => OFF
+#define DEBUG_MODE 		1
+
+#endif /* DEFINE_H_ */
