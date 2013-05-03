0836601d11a590905d25a23310b657058a54fae9
Gregory DEPUILLE <gregory.depuille@gmail.com>
2013-05-04 00:47:35 +0200

    Reglage

--- define.h
+++ define.h
@@ -14,16 +14,16 @@
 
 #define DEBUG_MODE
 
-#define TPS_MATCH			89500 // 89,5 sec pour palier au pb de réaction du bonhomme
-#define START_GONFLAGE		90500 // 90,5 sec pour démarrer le gonflage des ballon
-#define	PREPARE_GONFLAGE	88000 // Au bout de 88 sec on allume le gonfleur pour faire montée la pression
+#define TPS_MATCH			20500 // 89,5 sec pour palier au pb de réaction du bonhomme
+#define START_GONFLAGE		21500 // 90,5 sec pour démarrer le gonflage des ballon
+#define	PREPARE_GONFLAGE	22000 // Au bout de 88 sec on allume le gonfleur pour faire montée la pression
 
 // --------------------------------- //
 // Configuration de l'asservissement //
 // --------------------------------- //
 
-#define CYCLE_ASSERV				78			// Asserv 128 us * 78 = 9,984 ms
-#define TIME_ASSERV					0.009984	// En s
+//#define CYCLE_ASSERV				78			// Asserv 128 us * 78 = 9,984 ms
+//#define TIME_ASSERV					0.009984	// En s
 #define TIME_ASSERV_MS				10
 
 /*
@@ -47,12 +47,12 @@ POUR INFOS
 // Parametres PID //
 // -------------- //
 
-#define K_P_DISTANCE   		0.01
-#define K_I_DISTANCE   		0.00
+#define K_P_DISTANCE   		1.00
+#define K_I_DISTANCE   		0.10
 #define K_D_DISTANCE   		0.00
 
-#define K_P_ORIENTATION		0.01
-#define K_I_ORIENTATION		0.00
+#define K_P_ORIENTATION		1.00
+#define K_I_ORIENTATION		0.10
 #define K_D_ORIENTATION		0.00
 
 // ------------------------------- //
