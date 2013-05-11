3593a92a3d839414fbc7e939a1c7cb22bbdafa15
Gregory DEPUILLE <gregory.depuille@gmail.com>
2013-05-11 10:00:29 +0200

    Rampe plus petite pour avoir le temps de taper le dernier cadeaux.

--- main.cpp
+++ main.cpp
@@ -60,7 +60,7 @@ const int cdxStopOffset = 220;
 // Configuration des rampes //
 // ------------------------ //
 const int rampAccDistance = 300.0; // en mm/s2
-const int rampDecDistance = 200.0; // en mm/s2
+const int rampDecDistance = 100.0; // en mm/s2
 
 const double rampAccOrientation = 300.0; // en mm/s2
 const double rampDecOrientation = 200.0; // en mm/s2
